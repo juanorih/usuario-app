51e5358074fce5c8c71580816246fd19e3a0bba3
juan <juan.orih+git@gmail.com>
2023-11-22 23:37:07 -0600

    Implementacion Finalizada de UsuarioApp.

---
+++ app/src/main/java/com/acme/usuario/FragmentoInicio.java
@@ -0,0 +1,7 @@
+package com.acme.usuario;
+
+import androidx.fragment.app.Fragment;
+
+public class FragmentoInicio extends Fragment {
+    //    // TODO: En esta clase se implementa Fragmento Inicio.
+}
---
+++ app/src/main/java/com/acme/usuario/FragmentoLista.java
@@ -0,0 +1,74 @@
+package com.acme.usuario;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentoLista extends Fragment {
+
+    private UserAdapter userAdapter;
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_lista, container, false);
+
+//        String url = "http://10.0.2.2:8098/api/getUsers";
+        String url = "https://my-usuario-acme.onrender.com/api/getUsers"; // Reemplazar por la url desplegada en Render
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                List<User> users = new ArrayList<>();
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject userObject = response.getJSONObject(i);
+                        Integer id = userObject.getInt("id");
+                        String name = userObject.getString("name");
+                        String email = userObject.getString("email");
+                        users.add(new User(id, name, email));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                userAdapter = new UserAdapter(users, getContext());
+                RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+
+                recyclerView.setAdapter(userAdapter);
+
+                LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+                recyclerView.setLayoutManager(layoutManager);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity(), "Error al listar los datos " + error, Toast.LENGTH_SHORT).show();
+            }
+        });
+        queue.add(request);
+
+        return view;
+    }
+}
